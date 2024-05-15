e5d66e2b216b3bd6191de396ee4b7afceb64779d
addy204 <150568362+addy204@users.noreply.github.com>
2024-05-15 14:32:37 -0500

    codin1website

--- src/App.js
+++ src/App.js
@@ -26,14 +26,6 @@ function App() {
 
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>Codin 1</h1>
-//       </header>
-//     </div>
-//   );
-// }
+
 
 export default App;
